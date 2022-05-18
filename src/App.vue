2c70dedd6b2d25d8db9d0d550cd7685ed19bfb6c
jardenliu <jarden@qq.com>
2022-05-19 00:54:41 +0800

    chore: 🌈 added commitlint

---
+++ src/App.vue
@@ -0,0 +1,23 @@
+<template>
+    <div>
+        <input v-model="count"/>
+        {{count}}
+        <button @click="count++">+</button>
+        <button @click="count--">-</button>
+    </div>
+</template>
+
+<script lang="ts">
+import { defineComponent, ref } from "@vue/composition-api"
+import { useLocalStorage } from '@vueuse/core'
+export default defineComponent({
+    setup () {
+        const count = useLocalStorage('vue-count', 1)
+
+        return {
+            count
+        }
+    }
+})
+
+</script>
